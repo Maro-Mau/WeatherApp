f18749cc2cb3cb029e7f0cd50ea8ef8f3035dc6f
René Barthel <124598631+Maro-Mau@users.noreply.github.com>
2023-08-27 19:54:08 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly string apikey = "08f723768229d2538d67a045446ed88a";
+        private readonly string requestUri = "https://api.openweathermap.org/data/2.5/weather";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            HttpClient httpClient = new HttpClient();
+
+            var city = "Hanau";
+            var finaluri = requestUri + "?q=" + city + "&appid=" + apikey;
+            HttpResponseMessage httpresponse = httpClient.GetAsync(finaluri).Result;
+
+            string response = httpresponse.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(response);
+        }
+    }
+}
